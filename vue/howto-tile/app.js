935e9ab21d12c06e8645b0104d0158927d325dd9
Tim Buckland-Kerr <timbk86@gmail.com>
2019-04-22 18:13:42 +1000

    change title and % to vue, thumbnail broken

--- vue/howto-tile/app.js
+++ vue/howto-tile/app.js
@@ -4,32 +4,20 @@ let FeaturedItemFocus = {
     data() {
         return {
             "learning_plan": {
-                title: 'loading...'
-            }
+                title: 'loading...',
+                progress: 0
+            },
+            href: '#',
+            "background_image_url": ''
         }
     },
     created() {
         // fake api call
-        setTimeout(() =>
-            this.learning_plan = {
-                "id": 10551,
-                "title": "An introduction to the APF Learning Community",
-                "created_at": "2018-08-26T04:22:09.000Z",
-                "updated_at": "2018-08-30T01:32:32.000Z",
-                "items": [],
-                "progress": 29,
-                "thumbnail_url": "http://ganhri.fuseuniversal.com/media/W1siZiIsIjIwMTgvMDgvMzAvMDEvMzIvMzAvMzdmYmNiMGMtNDNiZS00NWJiLWE4NDEtNjZmNThmNzc4MjNhL2xlYXJuaW5nX3BsYW4ucG5nIl0sWyJwIiwidGh1bWIiLCIxNDN4MTA3IyJdXQ?sha=c152fbb77930fb60",
-                "thumbnail_2x_url": "http://ganhri.fuseuniversal.com/media/W1siZiIsIjIwMTgvMDgvMzAvMDEvMzIvMzAvMzdmYmNiMGMtNDNiZS00NWJiLWE4NDEtNjZmNThmNzc4MjNhL2xlYXJuaW5nX3BsYW4ucG5nIl0sWyJwIiwidGh1bWIiLCIyODZ4MjE0IyJdXQ?sha=f1976715af708048",
-                "thumbnail_3x_url": "http://ganhri.fuseuniversal.com/media/W1siZiIsIjIwMTgvMDgvMzAvMDEvMzIvMzAvMzdmYmNiMGMtNDNiZS00NWJiLWE4NDEtNjZmNThmNzc4MjNhL2xlYXJuaW5nX3BsYW4ucG5nIl0sWyJwIiwidGh1bWIiLCI0Mjl4MzIxIyJdXQ?sha=c7d1685dbb683425",
-                "expiration_info": {
-                    "expiry_state": "inactive",
-                    "expiry_date": null,
-                    "deadline_state": "disabled",
-                    "deadline_date": null,
-                    "reset_date": null
-                }
-            }, 100
-        )
+        const api_host = window.location.protocol + '//' + window.location.host
+        const thumbnail_url = data_local.learning_plan.thumbnail_2x_url.replace('http://','https://')
+        this.learning_plan = data_local.learning_plan
+        this.href = api_host + '/learning/plans/' + data_local.learning_plan.id
+        this.background_image_url = 'url("' + thumbnail_url + '")'
 
     }
 }
