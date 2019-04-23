dca3e7bbf57cecec2e33d970dfcce2e88d6de2c0
Tim Buckland-Kerr <timbk86@gmail.com>
2019-04-23 14:45:21 +1000

    fixed broken html tag at l-container,  + template

--- vue/community-learning_plans/community-learning_plans_app.js
+++ vue/community-learning_plans/community-learning_plans_app.js
@@ -1,3 +1,15 @@
+let CommunityLearningPlansItems = {
+    template: '#community-learning_plans_items-template',
+    data() {
+        return {
+            
+        }
+    },
+}
+
 new Vue({
-    el: '#community-learning_plans'
+    el: '#community-learning_plans',
+    components: {
+        'community-learning_plans_items': CommunityLearningPlansItems
+    }
 });
