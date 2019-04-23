b816528ad1ab3aff4eb0a37778d0fa8057dbce2b
Tim Buckland-Kerr <timbk86@gmail.com>
2019-04-23 15:14:43 +1000

    add list item vue component

--- vue/community-learning_plans/community-learning_plans_app.js
+++ vue/community-learning_plans/community-learning_plans_app.js
@@ -1,15 +1,22 @@
-let CommunityLearningPlansItems = {
-    template: '#community-learning_plans_items-template',
+let CommunityLearningPlansListItem = {
+    template: '#community-learning_plans_list-item-template'
+}
+
+let CommunityLearningPlansList = {
+    template: '#community-learning_plans_list-template',
     data() {
         return {
-            
+            testitems: [1,2,3,4,5]
         }
     },
+    components: {
+        'community-learning_plans_list-item': CommunityLearningPlansListItem
+    }
 }
 
 new Vue({
     el: '#community-learning_plans',
     components: {
-        'community-learning_plans_items': CommunityLearningPlansItems
+        'community-learning_plans_list': CommunityLearningPlansList
     }
 });
