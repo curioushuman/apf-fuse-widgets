2dd7c9ba57576be193a71ee1a662d37213d31200
Tim Buckland-Kerr <timbk86@gmail.com>
2019-04-29 19:33:41 +1000

    add html support to eslint, eslint howto tile

--- vue/howto-tile/app.js
+++ vue/howto-tile/app.js
@@ -1,52 +1,54 @@
 // featured item focus vue component
-let FeaturedItemFocus = {
-    template: '#featured-item-focus-template',
-    data() {
-        return {
-            "learning_plan": {
-                title: 'loading...',
-                progress: 0
-            },
-            href: '#',
-            "background_image_url": ''
-        }
-    },
-    created() {
-        // fake api call
-        const api_host = window.location.protocol + '//' + window.location.host
-        const thumbnail_url = data_local.learning_plan.thumbnail_2x_url.replace('http://','https://')
-        this.learning_plan = data_local.learning_plan
-        this.href = api_host + '/learning/plans/' + data_local.learning_plan.id
-        this.background_image_url = 'url("' + thumbnail_url + '")'
-        this.$emit('update-progress', this.learning_plan.progress > 0)
-
-    }
-}
+const FeaturedItemFocus = {
+  template: '#featured-item-focus-template',
+  data() {
+    return {
+      learning_plan: {
+        title: 'loading...',
+        progress: 0,
+      },
+      href: '#',
+      background_image_url: '',
+    };
+  },
+  created() {
+    // fake api call
+    const apiHost = window.location.protocol + '//' + window.location.host;
+    const thumbnailUrl = data_local.learning_plan.thumbnail_2x_url.replace(
+        'http://',
+        'https://'
+    );
+    this.learning_plan = data_local.learning_plan;
+    this.href = apiHost + '/learning/plans/' + data_local.learning_plan.id;
+    this.background_image_url = 'url("' + thumbnailUrl + '")';
+    this.$emit('update-progress', this.learning_plan.progress > 0);
+  },
+};
 
 // community link vue component
-let CommunityLink = {
-    template: '#community-link-template',
-    data() {
-        return {
-            communityLinkTestData: 'this is a community test!'
-        }
-    }
-}
+const CommunityLink = {
+  template: '#community-link-template',
+  data() {
+    return {
+      communityLinkTestData: 'this is a community test!',
+    };
+  },
+};
 
 new Vue({
-    el: '#howto-tile',
-    components: {
-        'featured-item-focus': FeaturedItemFocus,
-        'community-link': CommunityLink
+  el: '#howto-tile',
+  components: {
+    'featured-item-focus': FeaturedItemFocus,
+    'community-link': CommunityLink,
+  },
+  data() {
+    return {
+      progress: false,
+    };
+  },
+  methods: {
+    updateProgress: function(isProgress) {
+      this.progress = isProgress;
     },
-    data() {
-        return {
-            progress: false
-        }
-    },
-    methods: {
-        updateProgress: function (isProgress) {
-          this.progress = isProgress
-        }
-      }
-})
+  },
+});
