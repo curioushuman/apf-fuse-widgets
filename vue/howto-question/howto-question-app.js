84f9c8b199c1efd83b911c5f7f6409d61a08e05f
Tim Buckland-Kerr <timbk86@gmail.com>
2019-04-29 19:44:19 +1000

    eslint howto question

--- vue/howto-question/howto-question-app.js
+++ vue/howto-question/howto-question-app.js
@@ -1,40 +1,41 @@
-let CommunityLinkFaq = {
-    template: '#community-link-faq-template'
-}
+const CommunityLinkFaq = {
+  template: '#community-link-faq-template',
+};
 
-let CommunityLinkLibrary = {
-    template: '#community-link-library-template'
-}
-let CommunityLink = {
-    template: '#community-link-template'
-}
-let howtoQuestionForm = {
-    template: '#howto-question-form-template',
-    data() {
-        return {
-            question: 'Or ask your question here',
-            description: 'And add any additional information you might have, to help us find an answer'
-        }
+const CommunityLinkLibrary = {
+  template: '#community-link-library-template',
+};
+const CommunityLink = {
+  template: '#community-link-template',
+};
+const howtoQuestionForm = {
+  template: '#howto-question-form-template',
+  data() {
+    return {
+      question: 'Or ask your question here',
+      description:
+        'And add any additional information you might have,' +
+        'to help us find an answer',
+    };
+  },
+  methods: {
+    submitClicked: function() {
+      alert(this.question + '\n\n' + this.description);
     },
-    methods: {
-        submitClicked: function () {
-            alert(this.question + '\n\n' + this.description)
-        }
-    }
-}
-let howtoQuestionSuccess = {
-    template: '#howto-question-success-template'
-}
-
+  },
+};
+const howtoQuestionSuccess = {
+  template: '#howto-question-success-template',
+};
 
 // main vue instance
 new Vue({
-    el: '#howto-question',
-    components: {
-        'community-link-faq': CommunityLinkFaq,
-        'community-link-library': CommunityLinkLibrary,
-        'community-link': CommunityLink,
-        'howto-question-form': howtoQuestionForm,
-        'howto-question-success': howtoQuestionSuccess
-    }
+  el: '#howto-question',
+  components: {
+    'community-link-faq': CommunityLinkFaq,
+    'community-link-library': CommunityLinkLibrary,
+    'community-link': CommunityLink,
+    'howto-question-form': howtoQuestionForm,
+    'howto-question-success': howtoQuestionSuccess,
+  },
 });
